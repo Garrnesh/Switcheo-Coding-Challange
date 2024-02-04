8e276b689aff1c913b270f3c97e8a9e058f21bda
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2024-02-04 14:02:55 +0000

    Final changes to Problem 5

--- Problem 4/problem4.go
+++ Problem 4/problem4.go
@@ -3,11 +3,15 @@ package main
 import "fmt"
 
 func sum_to_n_a(n int) int {
+	// O(1) time complexity
+	// O(1) space complexity
 	result := int(((1 + n) * (n)) / 2)
 	return result
 }
 
 func sum_to_n_b(n int) int {
+	// O(n) time complexity
+	// O(1) space complexity
 	result := 0
 	for i := 1; i <= n; i++ {
 		result += i
@@ -16,6 +20,8 @@ func sum_to_n_b(n int) int {
 }
 
 func sum_to_n_c(n int) int {
+	// O(n) time complexity
+	// O(n) space complexity
 	if n == 1 {
 		return 1
 	}
--- Problem 5/CC/x/cc/keeper/query.go
+++
@@ -1,7 +0,0 @@
-package keeper
-
-import (
-	"CC/x/cc/types"
-)
-
-var _ types.QueryServer = Keeper{}
