cc935eb8c06e14c16bdd3a80cc82a01cb6eb7f92
Garrnesh <76935598+Garrnesh@users.noreply.github.com>
2024-02-04 15:23:24 +0000

    Pre edit

--- Problem 5 Updated/prob/x/prob/keeper/msg_server_create_exchange.go
+++ Problem 5 Updated/prob/x/prob/keeper/msg_server_create_exchange.go
@@ -11,11 +11,12 @@ import (
 func (k msgServer) CreateExchange(goCtx context.Context, msg *types.MsgCreateExchange) (*types.MsgCreateExchangeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	var exchange = types.Exchange{
-		Date:     msg.Date,
-		Amount:   msg.Amount,
-		Message:  msg.Message,
-		Reciever: msg.Reciever,
-		Sender:   msg.Creator,
+		Date:           msg.Date,
+		Amount:         msg.Amount,
+		Message:        msg.Message,
+		Reciever:       msg.Reciever,
+		BreakConsensus: msg.BreakConsensus,
+		Sender:         msg.Creator,
 	}
 	id := k.AppendExchange(
 		ctx,
